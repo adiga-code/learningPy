5fee465f136e3186519abe6ed493b446754c55d7
AdiGaMan07 <xewevdm@gmail.com>
2021-02-09 22:04:29 +0300

    9.02.21

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
-print("GGG")
-
-
-
-
+class Dog():
+	"""Простая модель собаки"""
+	def __init__(self , name , age):
+		self.name = name
+		self.age = age
+	def sit(self):
+		print(f"{self.name} is now sitting. ")
+	def roll_over(self):
+		print(f"{self.name} roll over! ")
+my_dog = Dog("Dgek", 11)
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age}.")
 
 
 
