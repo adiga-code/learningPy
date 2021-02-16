b5d8034dd6a34136509087aa4e6d6873c05a85f7
AdiGaMan07 <xewevdm@gmail.com>
2021-02-16 21:43:52 +0300

    second commit

--- test.py
+++ test.py
@@ -7,9 +7,14 @@ class Dog():
 		print(f"{self.name} is now sitting. ")
 	def roll_over(self):
 		print(f"{self.name} roll over! ")
+"""Создаём экземпляр класса"""		
 my_dog = Dog("Dgek", 11)
+"""Обращение к атрибутам"""
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age}.")
+"""Вызов методов"""
+my_dog.sit()
+my_dog.roll_over()
 
 
 
