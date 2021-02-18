e80a1bf56b8ca5fc52164daa919855bdd4fee2f7
AdiGaMan07 <xewevdm@gmail.com>
2021-02-18 21:25:42 +0300

    fourth commit

--- test.py
+++ test.py
@@ -3,19 +3,28 @@ class Dog():
 	def __init__(self , name , age):
 		self.name = name
 		self.age = age
+
 	def sit(self):
 		print(f"{self.name} is now sitting. ")
+
 	def roll_over(self):
 		print(f"{self.name} roll over! ")
+
 """Создаём экземпляр класса"""		
 my_dog = Dog("Dgek", 11)
+your_dog = Dog("Bobik", 3)
 """Обращение к атрибутам"""
 print(f"My dog's name is {my_dog.name}.")
 print(f"My dog is {my_dog.age}.")
+
+print(f"\nYour dog's name is {your_dog.name}.")
+print(f"Your dog is {your_dog.age}")
+
 """Вызов методов"""
 my_dog.sit()
 my_dog.roll_over()
-
+your_dog.sit()
+your_dog.roll_over()
 
 
 
