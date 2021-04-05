e0081fcbd05344ed229136c30748d4818b428f76
AdiGaMan07 <xewevdm@gmail.com>
2021-04-05 22:12:27 +0300

    eleventh commit

--- test.py
+++ test.py
@@ -1,26 +1,68 @@
-class Restaurant():
-    def __init__(self , restaurant_name , cuisine_type):
-        self.restaurant_name = restaurant_name
-        self.cuisine_type = cuisine_type
-    def describe_restaurant(self):
-        print(f"Restaurant '{self.restaurant_name} is good.")
-    def open_restaurant(self):
-        print(f"Restaurant {self.restaurant_name} has {self.cuisine_type} type.")
-#класс-потомок класса Restaurant
-class IceCreamStand(Restaurant):
-    def __init__(self, restaurant_name, cuisine_type):
-        super().__init__(restaurant_name, cuisine_type)
-        self.flavors = ['chocolate', 'vanila', 'fistashki']
-    def describe_flavors(self):
-        print(f"\nWe have this flavors:")
-        for flavor in self.flavors:
-            print(flavor) 
+class Car():
 
-restaurant1 = IceCreamStand('Ice creams', 'kafe ice cream')
-print(restaurant1.open_restaurant())
-restaurant1.describe_flavors()
+    """Простая модель автомобиля"""
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometr_reading = 0
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    def read_odometr(self):
+        print(f"This car has {self.odometr_reading} kilometrs on it")
+    def update_odometr(self, kilometrs):
+        if kilometrs >= self.odometr_reading:
+            self.odometr_reading = kilometrs
+        else:
+            print("You can't roll back an odometr!!!")
+    def increment_odometr(self, kilometr):
+        self.odometr_reading += kilometr
 
+class Battery():
+    """Простая модель аккумулятора автомобиля"""
+    def __init__(self, battery_size=75):
+        self.battery_size = battery_size
+    def describe_battery(self):
+        """Выводит сообщение о мощности аккумулятора"""
+        print(f"This car has {self.battery_size}-kWh battery.")
+    def upgrade_battery(self, number):
+        if self.battery_size > number:
+            self.battery_size += number
+        else:
+            self.battery_size = 100
+            print("OI OI OI") 
+    def get_range(self):
+        """Выводит приблизительный запас хода для аккумулятора"""
+        if self.battery_size == 60:
+            ran_ge = 260
+        elif self.battery_size == 100:
+            ran_ge = 315
+        else:
+            ran_ge = 300
 
+        print(f'This car can go about {ran_ge} miles on a full charge')
+
+class ElectricCar(Car):
+
+    """Представляет аспекты машин, специфические для электрокаров"""
+    def __init__(self, make, model, year):
+        """
+        Инициализирует атрибуты класса-родителя
+        Затем инициализирует атрибуты, специфические для электромобиля
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+    def fill_gas_tank(self):
+        """У электромобилей нет бензобака"""
+        print("This car don't need a gas tank!")
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery(25)
+my_tesla.battery.get_range()
 
 
 
--- Сделанные задачи(с книги).py
+++ Сделанные задачи(с книги).py
@@ -1528,7 +1528,7 @@ restaurant1.describe_flavors()
 
 
 
-# Nom 9.8
+# Nom 9.7
 
 #1)С упражнением 9.3 сделать то же, что и с 9.1 в упражнении 9.6
 class User():
@@ -1556,6 +1556,74 @@ admin1.greet_user()
 admin1.show_privilegs()
 
 
+
+# Nom 9.9
+#1)В программу про машины, добавить метод, который устанавливает емкость батареи
+class Car():
+
+    """Простая модель автомобиля"""
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometr_reading = 0
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    def read_odometr(self):
+        print(f"This car has {self.odometr_reading} kilometrs on it")
+    def update_odometr(self, kilometrs):
+        if kilometrs >= self.odometr_reading:
+            self.odometr_reading = kilometrs
+        else:
+            print("You can't roll back an odometr!!!")
+    def increment_odometr(self, kilometr):
+        self.odometr_reading += kilometr
+
+class Battery():
+    """Простая модель аккумулятора автомобиля"""
+    def __init__(self, battery_size=75):
+        self.battery_size = battery_size
+    def describe_battery(self):
+        """Выводит сообщение о мощности аккумулятора"""
+        print(f"This car has {self.battery_size}-kWh battery.")
+    def upgrade_battery(self, number):
+        if self.battery_size > number:
+            self.battery_size += number
+        else:
+            self.battery_size = 100
+            print("OI OI OI") 
+    def get_range(self):
+        """Выводит приблизительный запас хода для аккумулятора"""
+        if self.battery_size == 60:
+            ran_ge = 260
+        elif self.battery_size == 100:
+            ran_ge = 315
+        else:
+            ran_ge = 300
+
+        print(f'This car can go about {ran_ge} miles on a full charge')
+
+class ElectricCar(Car):
+
+    """Представляет аспекты машин, специфические для электрокаров"""
+    def __init__(self, make, model, year):
+        """
+        Инициализирует атрибуты класса-родителя
+        Затем инициализирует атрибуты, специфические для электромобиля
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+    def fill_gas_tank(self):
+        """У электромобилей нет бензобака"""
+        print("This car don't need a gas tank!")
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery(25)
+my_tesla.battery.get_range()
 
 
 
